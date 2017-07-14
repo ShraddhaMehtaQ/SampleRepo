047a01580e7251843bfc9764f4926cfc237b7fb0
Shraddha Mehta <shraddha.mehta@quantiphi.com>
2017-07-14 14:03:13 +0530

    OOP

---
+++ Day 4/Classes.py
@@ -0,0 +1,22 @@
+class Car():
+
+	def __init__(self, make, model, year):
+		print self
+		self.make = make
+		self.model = model
+		self.year = year
+		self.fuel_capacity = 15
+		self.fuel_level = 0
+	
+	def fill_tank(self):
+		self.fuel_level = self.fuel_capacity
+		print("Fuel tank is full.")
+	
+	def drive(self):
+		print("The car is moving.")
+
+my_car = Car('audi', 'a4', 2016)
+print(my_car.make)
+print(my_car.model)
+print(my_car.year)
+my_car.fill_tank()
